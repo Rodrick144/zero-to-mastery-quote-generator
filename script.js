cf9f8f4a94c7ec183f2f6294ab975f774da3ea12
Miner <rodrick.heffleydumb@gmail.com>
2023-05-10 12:13:00 -0400

    the engine has served its purpose

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const thisIsATest = document.getElementById('waitIDidntNeedThat');
 
 // console.log(legalGuesses2[12]);
 
-thisIsATest.textContent = legalGuesses3[13];
+thisIsATest.textContent = possibleWords3[13];
